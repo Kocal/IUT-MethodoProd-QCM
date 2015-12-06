2072a3f64824ac58c9cf27a075f9ec26cb19a183
ALLIAUME Hugo <kocal@live.fr>
2015-12-06 16:08:39 +0100

    Ajout de la table "subjects" et ajout d'une clée étrangère dans la table "qcms"
    :-)

--- database/migrations/2015_11_18_124209_create_qcms_table.php
+++ database/migrations/2015_11_18_124209_create_qcms_table.php
@@ -14,6 +14,7 @@ class CreateQcmsTable extends Migration
         Schema::create('qcms', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->integer('subject_id')->unsigned();
             $table->string('name');
             $table->mediumText('description');
             $table->timestamps();
