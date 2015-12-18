4dec06c1658b53e36cbb047ca4c28a027d091ad1
ALLIAUME Hugo <kocal@live.fr>
2015-12-19 00:39:08 +0100

    Ajout de la méthode Subject::toList()
    Cette méthode récupère les matières dans la base de données, trie les
    matières par leur nom (ordre alphabétique), puis renvoit une liste sous
    la forme ['id' => 'name']

--- app/Subject.php
+++ app/Subject.php
@@ -3,8 +3,26 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
-class Subject extends Model
-{
+class Subject extends Model {
+
     protected $fillable = ['slug', 'name'];
+
+    /**
+     * Retourne une liste contenant les Matières sous la forme [$id] => $name
+     *
+     * @return array
+     */
+    public static function toList() {
+        $list = [];
+        $subjects = DB::table('subjects')->orderBy('name')->get(['id', 'name']);
+
+        foreach($subjects as $subject) {
+            $list[$subject->id] = $subject->name;
+        }
+
+        return $list;
+    }
+
 }
