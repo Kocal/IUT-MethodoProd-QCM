6a5aa615fb8329bde020719ed7dba5bd030344d7
ALLIAUME Hugo <kocal@live.fr>
2015-12-24 18:32:46 +0100

    méééé pk tu trol??? :'( :' (:'(

--- resources/views/partials/mustache_template_question.php
+++ resources/views/partials/mustache_template_question.php
@@ -5,7 +5,7 @@
             <span title="Supprimer la question n°{{ questionNumberDisplay }}"
                   class="btn-remove-question glyphicon-remove"></span>
     </legend>
-    <?= BootForm::textarea('Énoncé' . $requiredField, 'questions[ {{questionNumber}} ]')->rows(2)->required(); ?>
+    <?= BootForm::textarea('Énoncé' . $requiredField, 'questions[{{questionNumber}}]')->rows(2)->required(); ?>
 
     <table class="table-qcm table-responsive">
         <thead>
