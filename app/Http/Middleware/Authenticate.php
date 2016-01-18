06dc1f2f02f2cb34cd059be6f712ae3e7e06460d
ALLIAUME Hugo <kocal@live.fr>
2016-01-18 11:26:48 +0100

    Ajout d'un message afin d'informer l'utilisateur pourquoi l'accès a été refusé

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Session;
 
 class Authenticate
 {
@@ -17,7 +18,8 @@ class Authenticate
     /**
      * Create a new filter instance.
      *
-     * @param  Guard  $auth
+     * @param  Guard $auth
+     *
      * @return void
      */
     public function __construct(Guard $auth)
@@ -28,8 +30,9 @@ class Authenticate
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -38,6 +41,11 @@ class Authenticate
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             } else {
+                Session::push(
+                    'messages',
+                    'danger|Vous devez être connecté pour accéder à cette page'
+                );
+
                 return redirect()->guest('auth/login');
             }
         }
