652fa9d5812b9b171156ce6af0f6f7e71eb7299c
ALLIAUME Hugo <kocal@live.fr>
2016-01-19 09:49:15 +0100

    Ajout du champ "participations.qcm_id"

--- database/migrations/2015_11_18_161536_create_participations_table.php
+++ database/migrations/2015_11_18_161536_create_participations_table.php
@@ -14,6 +14,7 @@ class CreateParticipationsTable extends Migration
         Schema::create('participations', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->integer('qcm_id')->unsigned();
             $table->integer('question_id')->unsigned();
             $table->integer('answer_id')->unsigned();
         });
